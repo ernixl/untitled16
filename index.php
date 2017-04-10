0431dff7a464c5e8a71219aebe5a1b864917e91e
ernixl <ernixl@yahoo.com>
2017-04-10 22:02:35 +0100

    yes

--- index.php
+++ index.php
@@ -1,43 +1,18 @@
 <!DOCTYPE html>
 <html>
 <head>
-    ffffffffffffff
+    ffffffffffffffkkkkkkkkkkkkk
 </head>
 <body>
 <p>
-    <?
-    $connectstr_dbhost = '';
-    $connectstr_dbname = '';
-    $connectstr_dbusername = '';
-    $connectstr_dbpassword = '';
-    foreach ($_SERVER as $key => $value) {
-        if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
-            continue;
-        }
-        $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
-        $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
-        $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
-        $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
-    }
-    $link = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword, $connectstr_dbname);
-    if (!$link) {
-        echo "Error: Unable to connect to MySQL." . PHP_EOL;
-        echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-        echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-        exit;
-    }
-    $sql_query = "SELECT * FROM marvelmovies  WHERE yearReleased = 2005";
-    // execute the SQL query
-    $result = $db->query($sql_query);
+    <?php
 
-    while ($row = $result->fetch_array()) {
-// process the $row
-    }
-    while ($row = $result->fetch_array()) {
-        // print out fields from row of data
-        echo "<p>" . $row['title'] . "</p>";
-    }
-
+    $result=mysqli_query($db,$sql);
+    while($row = $result->fetch_array())
+    {
+      $movieTitle = $row['title'];
+echo "<p>" . $movieTitle . "</p>";
+}
 
     ?>
 
