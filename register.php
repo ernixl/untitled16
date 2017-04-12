b11c3bd37a2fca6e2ac60ece1ecc8c8d351a5c18
ernixl <ernixl@yahoo.com>
2017-04-12 09:10:08 +0100

    yes

--- register.php
+++ register.php
@@ -1,7 +1,59 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: ernes
- * Date: 12/04/2017
- * Time: 08:28
- */
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Register your details</title>
+</head>
+<body>
+<h1>Register to login</h1>
+<div>
+    <h3>Register to login</h3>
+
+    <form>
+        <table>
+
+            <tr>
+                <td>name:</td>
+                <td><input type="text" name="name" class="textInput"></td>
+            </tr>
+
+            <tr>
+                <td>surname:</td>
+                <td><input type="text" name="surname" class="textInput"></td>
+            </tr>
+            <tr>
+                <td>username:</td>
+                <td><input type="text" name="username" class="textInput"></td>
+            </tr>
+
+            <tr>
+                <td>email:</td>
+                <td><input type="email" name="email" class="textInput"></td>
+            </tr>
+
+            <tr>
+                <td>password:</td>
+                <td><input type="password" name="password" class="textInput"></td>
+            </tr>
+
+            <tr>
+                <td>password:</td>
+                <td><input type="password" name="password2" class="textInput"></td>
+            </tr>
+
+            <tr>
+                <td>submit:</td>
+                <td><input type="submit" name="submit" class="textInput"></td>
+            </tr>
+
+
+        </table>
+
+
+    </form>
+</div>
+
+
+</body>
+
+
+</html>
