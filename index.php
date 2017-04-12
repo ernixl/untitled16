595eb84a9a374f3e3ac6f77ef94464f24b8985d6
ernixl <ernixl@yahoo.com>
2017-04-12 06:13:09 +0100

    yes

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 <html>
 <meta charset="utf-8">
 <head>
-        <title>PHP Login Form without Session</title>
-        <link rel ="stylesheet" href ="style.css" type="text/css" />
+    <title>PHP Login Form without Session</title>
+    <link rel="stylesheet" href="style.css" type="text/css"/>
 </head>
 
 <body>
@@ -11,13 +11,13 @@
 <h1>PHP Login Form with Session</h1>
 <div class="loginBox" id="btn">
     <h3>Login Form</h3>
-    <br><br >
-    <form method = "post" id="form" action = "login.php">
-        <label>User name: </label><br >
-        <input type="text" name="username" placeholder="username" /><br><br>
+    <br><br>
+    <form method="post" id="form" action="login.php">
+        <label>User name: </label><br>
+        <input type="text" name="username" placeholder="username"/><br><br>
         <label>Password: </label><br>
-        <input type="password" name="password" placeholder ="password" /> <br><br>
-        <input type = "submit" id="submit" name = "submit" value = "login"/>
+        <input type="password" name="password" placeholder="password"/> <br><br>
+        <input type="submit" id="submit" name="submit" value="login"/>
     </form>
     <div class="error"><?php // echo $err or; ?><?php // echo $user name; echo $passwor d; ?></div>
 </div>
