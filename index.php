c3c42033a6b17dc627c191b021e9098cc6a00771
ernixl <ernixl@yahoo.com>
2017-04-17 09:50:06 +0100

    yes

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 
     <h3>Enter your login information or click <a href="register.php">here</a> to register</h3>
     <br>
-    <form method="post" action="index.php">
+    <form method="post" action="login.php">
         <table>
 
             <tr>
@@ -35,35 +35,7 @@
     </form>
     <div class="error"><?php // echo $err or; ?><?php // echo $user name; echo $passwor d; ?></div>
 </div>]
-<?php
-$DB_SERVER = "127.0.0.1:57075";
-$DB_USERNAME = "root";
-$DB_PASSWORD = "password";
-$DB_DATABASE = "login";
 
-$db = mysqli_connect($DB_SERVER, $DB_USERNAME, $DB_PASSWORD, $DB_DATABASE);
-
-
-if (empty($_POST["username"]) || empty($_POST["password"])) {
-echo "Both fields are required";
-} else {
-$username = $_POST['username'];
-$password = $_POST['password'];
-
-$sql = "SELECT uid FROM users WHERE username = '$username' and
-password ='$password' ";
-
-$result = mysqli_query($db, $sql);
-
-if (mysqli_num_rows($result) == 1) {
-header("location: homepage.php"); // Redi recti ng To anot her Page
-} else {
-echo "Incorrect username or passwords. ";
-}
-
-}
-
-?>
 </body>
 
 </html>
