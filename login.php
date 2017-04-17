4fb8871cfab20971b22df1f6b2238d8772d6d902
ernixl <ernixl@yahoo.com>
2017-04-17 20:35:27 +0100

    yes

--- login.php
+++ login.php
@@ -13,9 +13,19 @@ $sql = " SELECT id FROM login WHERE uid='$uid' and pwd='$pwd' ";
 
 $result = mysqli_query($db,$sql);
 
-if (mysqli_num_rows($result) == 1) {
-    header("location: homepage.php");
-} else {
-    echo "Incorrect username password. ";
-}}
+     if (mysql_num_rows($result)==1) {
+         echo "only 1 record in database";
+         session_start();
+         $_SESSION["uid"] = $uid;
+         $_SESSION["pwd"] = $pwd;
+
+         $row = mysqli_fetch_array($result);
+         if ($row['admin'] == 0) {
+             echo "admin user";
+         } elseif ($row['admin'] == 1) {
+             echo "normal user";
+         } else
+             echo "invalid password and username";
+     }}
+
 ?>
