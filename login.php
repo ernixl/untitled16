ce08a274dc450892c47f1433cfcfa206f42df4f1
ernixl <ernixl@yahoo.com>
2017-04-17 20:37:20 +0100

    yes

--- login.php
+++ login.php
@@ -9,16 +9,11 @@ if(empty($_POST[ "uid"]) || empty($_POST[ "pwd"]))
 $uid = $_POST['uid'];
 $pwd = $_POST['pwd'];
 
-$sql = " SELECT id FROM login WHERE uid='$uid' and pwd='$pwd' ";
+$sql = " SELECT * FROM login WHERE uid='$uid' and pwd='$pwd' ";
 
 $result = mysqli_query($db,$sql);
 
      if (mysql_num_rows($result)==1) {
-         echo "only 1 record in database";
-         session_start();
-         $_SESSION["uid"] = $uid;
-         $_SESSION["pwd"] = $pwd;
-
          $row = mysqli_fetch_array($result);
          if ($row['admin'] == 0) {
              echo "admin user";
