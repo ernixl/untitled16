707ae480d16204d2b08094625c36dd162de388d3
ernixl <ernixl@yahoo.com>
2017-04-17 14:47:33 +0100

    yes

--- db_connect.php
+++ db_connect.php
@@ -1,6 +1,6 @@
 <?php
 
-$db = mysqli_connect("127.0.0.1:57075", "root", "null", "users");
+$db = mysqli_connect("127.0.0.1:57075", "root", "", "users");
 
 if(!$db){
     die("Connection failed".mysqli_connect_error());
