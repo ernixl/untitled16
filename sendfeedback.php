bdeee62947dfef38d0da0c8d20a5f2559c1b8a1b
ernixl <ernixl@yahoo.com>
2017-04-17 23:28:22 +0100

    yes

--- sendfeedback.php
+++ sendfeedback.php
@@ -12,8 +12,8 @@
         <input type="text" name="last" placeholder="Lastname"><br><br>
         <input type="text" name="group" placeholder="Groupname"><br><br>
         <input type="text" name="course" placeholder="Course id"><br><br>
+        <textarea name="feedback" placeholder="write your feedback here" id="" cols="30" rows="10"></textarea>
         <input type="file" name="image" placeholder="Upload">
-        <input name="feedback" name="feedback" placeholder="write your feedback here" id="" cols="30" rows="10"></input>
         <input type="submit" value="Send" id="submit">
 
 
