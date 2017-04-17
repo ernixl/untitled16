494b385fe1252f3b373fb7b2b80c2f294b40c2c8
ernixl <ernixl@yahoo.com>
2017-04-17 09:27:46 +0100

    yes

--- upload.php
+++ upload.php
@@ -1,7 +1,12 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: ernes
- * Date: 17/04/2017
- * Time: 08:58
- */
+<!DOCTYPE html>
+<html>
+<body>
+
+<form action="upload.php" method="post" enctype="multipart/form-data">
+    Select image to upload:
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="submit" value="Upload Image" name="submit">
+</form>
+
+</body>
+</html>
