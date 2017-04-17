37d01ffecf5901f9a898cc3008ef14ecb3e3416f
ernixl <ernixl@yahoo.com>
2017-04-17 12:38:13 +0100

    yes

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 $DB_SERVER = "127.0.0.1:57075";
 $DB_USERNAME = "root";
 $DB_PASSWORD = "password";
-$DB_DATABASE = "peerevaluation";
+$DB_DATABASE = "coursework";
 
 $db = mysqli_connect($DB_SERVER, $DB_USERNAME, $DB_PASSWORD, $DB_DATABASE);
 
