34a68dde404d22f96a7fea49102d3346a3c839a3
ernixl <ernixl@yahoo.com>
2017-04-17 20:42:23 +0100

    yes

--- login.php
+++ login.php
@@ -5,7 +5,7 @@ include 'connect.php';
 $uid = $_POST['uid'];
 $pwd = $_POST['pwd'];
 
-$sql = " SELECT uid FROM login WHERE uid='$uid' and pwd='$pwd' ";
+$sql = " SELECT id FROM login WHERE uid='$uid' and pwd='$pwd' ";
 
 $result = mysqli_query($db,$sql);
 
