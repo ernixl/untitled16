0d3e2759bf4859d5982e90fcba49020798b8f86b
ernixl <ernixl@yahoo.com>
2017-04-17 18:58:27 +0100

    yes

--- login.php
+++ login.php
@@ -2,22 +2,18 @@
 
 include 'connect.php';
 
-    $sql = "SELECT uid FROM login WHERE uid = '$uid' and
+$sql = "SELECT uid FROM login WHERE uid = '$uid' and
 pwd ='$pwd' ";
 
-    $result = mysqli_query($db, $sql);
+$result = mysqli_query($db, $sql);
 
-    if ($result == true) {
-        session_start();
-        $_SESSION["uid"] = $uid;
-        $_SESSION["pwd"] = $pwd;
-
-        $row = mysqli_fetch_array($result);
-        if ($row['admin'] == 0) {
-            header("location: homepge.php");
-        } elseif ($row['admin'] == 1) {
-            header("location:admin.php");
-        }
-    } else
-        echo "invalid password and username";
+if ($result == true) {
+    $row = mysqli_fetch_array($result);
+    if ($row['admin'] == 0) {
+        header("location: homepge.php");
+    } elseif ($row['admin'] == 1) {
+        header("location:admin.php");
+    }
+} else
+    echo "invalid password and username";
 ?>
