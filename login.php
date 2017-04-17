e15085b4aa2e85c0d8cdce8feef57b26e8d65b5c
ernixl <ernixl@yahoo.com>
2017-04-17 20:27:29 +0100

    yes

--- login.php
+++ login.php
@@ -1,11 +1,15 @@
 <?php
 
 include 'connect.php';
-
+if(empty($_POST[ "uid"]) || empty($_POST[ "pwd"]))
+ {
+     echo "Both fields are required. ";
+ }else
+ {
 $uid = $_POST['uid'];
 $pwd = $_POST['pwd'];
 
-$sql = "SELECT * FROM login WHERE uid = '$uid' and pwd ='$pwd' ";
+$sql = " SELECT id FROM login WHERE uid='$uid' and pwd='$pwd' ";
 
 $result = mysqli_query($db,$sql);
 
@@ -13,5 +17,5 @@ if (mysqli_num_rows($result) == 1) {
     header("location: homepage.php");
 } else {
     echo "Incorrect username password. ";
-}
+}}
 ?>
