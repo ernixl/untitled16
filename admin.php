e4756b7c29e675ff9d1a214f66884ece05486c44
ernixl <ernixl@yahoo.com>
2017-04-17 09:44:14 +0100

    yes

--- admin.php
+++ admin.php
@@ -1,7 +1,21 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: ernes
- * Date: 17/04/2017
- * Time: 08:43
- */
+<html>
+<head>
+    <meta charset =" utf-8">
+    <title>Welcome</title>
+    <link rel ="stylesheet" href ="stylesheet.css" type="text/ css" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+
+<header>
+    <h1>Hello</h1>
+</header>
+
+<a href="index.php">Home</a>
+<a href="viewfeedback.php">sendfeedback</a>
+<a href="viewupload.php">upload file</a>
+<a href="setcoursework.php">setcoursework</a>
+
+</body>
+</html>
