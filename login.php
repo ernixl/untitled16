83b69a1b07a92ad3ee578413e708e3d509fadbcb
ernixl <ernixl@yahoo.com>
2017-04-17 11:52:18 +0100

    yes

--- login.php
+++ login.php
@@ -8,7 +8,7 @@ $db = mysqli_connect($DB_SERVER, $DB_USERNAME, $DB_PASSWORD, $DB_DATABASE);
 
 
 if (empty($_POST["username"]) || empty($_POST["password"])) {
-    echo "Incorrect username or password";
+    echo "Go back and enter a correct username and password";
 } else {
     $username = $_POST['username'];
     $password = $_POST['password'];
