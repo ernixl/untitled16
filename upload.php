5505d7b09aad034c7ec172bf301f3b8fbd0eb053
ernixl <ernixl@yahoo.com>
2017-04-17 09:31:47 +0100

    yes

--- upload.php
+++ upload.php
@@ -1,6 +1,12 @@
 <!DOCTYPE html>
 <html>
-<body>
+<head>
+    <title>Register your details</title>
+    <link rel="stylesheet" href="style.css" type="text/css"/>
+</head>
+<body id="body">
+<h1>You can give upload files here</h1>
+<div id="form">
 
 <form action="upload.php" method="post" enctype="multipart/form-data">
     Select image to upload:
