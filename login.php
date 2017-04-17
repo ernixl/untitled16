476a38a281950007dd926927ec5535d84b375290
ernixl <ernixl@yahoo.com>
2017-04-17 22:17:56 +0100

    yes

--- login.php
+++ login.php
@@ -10,14 +10,14 @@ $sql = "SELECT uid FROM login WHERE username='$username' and password='$password
 $result = mysqli_query($db, $sql);
 $row = mysqli_fetch_array($result);
 
-if (mysqli_num_rows($result) == 1) {
-    if ($row['admin'] == 0) {
+if (mysqli_num_rows($result) == 1 && $row['admin'] == 0) {
         header("location: homepage.php");
-    } elseif ($row['admin'] == 1) {
+    } elseif (mysqli_num_rows($result) == 1 && $row['admin'] == 1) {
         header("Location: admin.php");}
+
         else
         echo "Incorrect username or password. ";
-}
+
 
 ?>
 
