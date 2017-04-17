336e01eeab43fe2fcca210c1e597d3ef5773afb9
ernixl <ernixl@yahoo.com>
2017-04-17 20:14:22 +0100

    yes

--- login.php
+++ login.php
@@ -7,11 +7,11 @@ $pwd = $_POST['pwd'];
 
 $sql = "SELECT uid FROM login WHERE uid = '$uid' and pwd ='$pwd' ";
 
-$result = mysqli_query($db, $sql);
+$result = mysqli_query($db,$sql);
 
 if (mysqli_num_rows($result) == 1) {
     header("location: homepage.php");
 } else {
-    echo "Incorrect username or password. ";
+    echo "Incorrect username password. ";
 }
 ?>
