a391bd7887c268803c851a47f066073e76772432
ernixl <ernixl@yahoo.com>
2017-04-17 15:14:36 +0100

    yes

--- signup.php
+++ signup.php
@@ -9,7 +9,7 @@ $uid = $_POST['uid'];
 $pwd = $_POST['pwd'];
 $pwd2 = $_POST['pwd2'];
 
-$sql = "INSERT INTO users ('first', 'last', 'email', 'uid', 'pwd')
+$sql = "INSERT INTO users ('first','last','email','uid','pwd')
 VALUES ('$first','$last','$email','$uid','$pwd')";
 
 $result = $db->query($sql);
