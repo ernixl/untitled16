9877bd866325a4b31107b12a6dbf3f8b3eb2ac12
ernixl <ernixl@yahoo.com>
2017-04-17 14:18:08 +0100

    yes

--- db_connect.php
+++ db_connect.php
@@ -1,5 +1,8 @@
 <?php
 
 $db = mysqli_connect("127.0.0.1:57075", "root", "password", "users");
-if(!$db)
+
+if(!$db){
+    die("Connection failed".mysqli_connect_error());
+}
 ?>
--- signup.php
+++ signup.php
@@ -1,7 +1,16 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: ernes
- * Date: 17/04/2017
- * Time: 13:34
- */
+
+include 'db_connect.php';
+
+$first = $_POST['first'];
+$last = $_POST['last'];
+$email = $_POST['email'];
+$uid = $_POST['uid'];
+$pwd = $_POST['pwd'];
+$pwd2 = $_POST['pwd2'];
+
+echo $first . "<br>";
+echo $last . "<br>";
+echo $email . "<br>";
+echo $uid . "<br>";
+echo $pwd . "<br>";
