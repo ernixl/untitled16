87f300a89cc3e1e30f84a178d6171be9a58ab729
ernixl <ernixl@yahoo.com>
2017-04-17 12:00:27 +0100

    yes

--- homepage.php
+++ homepage.php
@@ -2,15 +2,15 @@
 <head>
     <meta charset =" utf-8">
     <title>Welcome</title>
-    <link rel ="stylesheet" href ="stylesheet.css" type="text/ css" />
+    <link rel="stylesheet" href="style.css" type="text/css"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 
 <body id="body">
-
+<h1>Hello</h1>
 
     <a href="index.php">Logout</a>
-<h1>Hello</h1>
+
 
 
 <a href="index.php">Home</a>
