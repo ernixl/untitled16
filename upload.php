4b32a92941ae2a1f7715da0121a872d2de16a375
ernixl <ernixl@yahoo.com>
2017-04-17 09:32:25 +0100

    yes

--- upload.php
+++ upload.php
@@ -10,7 +10,7 @@
 
 <form action="upload.php" method="post" enctype="multipart/form-data">
     Select image to upload:
-    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="file" name="fileToUpload" id="fileToUpload"><br><br>
     <input type="submit" value="Upload Image" name="submit">
 </form>
 
