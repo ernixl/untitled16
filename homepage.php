426807aa69e9e1c145cc740f96cd751c03505707
ernixl <ernixl@yahoo.com>
2017-04-17 19:00:01 +0100

    yes

--- homepage.php
+++ homepage.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-    <meta charset =" utf-8">
+    <meta charset=" utf-8">
     <title>Welcome</title>
     <link rel="stylesheet" href="style.css" type="text/css"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,11 +9,6 @@
 <body id="body">
 <a href="index.php">Logout</a>
 <h1>Hello</h1>
-
-
-
-
-
 <a href="index.php">Home</a>
 <a href="sendfeedback.php">sendfeedback</a>
 <a href="upload.php">upload file</a>
