9b351e473c68125ed7d7c5e889945214add5dc18
ernixl <ernixl@yahoo.com>
2017-04-17 15:42:30 +0100

    yes

--- signup.php
+++ signup.php
@@ -9,8 +9,10 @@ $uid = $_POST['uid'];
 $pwd = $_POST['pwd'];
 $pwd2 = $_POST['pwd2'];
 
+if ($pwd==$pwd2){
 $sql = "INSERT INTO login (first, last, email, uid, pwd)
-VALUES ('$first','$last','$email','$uid','$pwd')";
+VALUES ('$first','$last','$email','$uid','$pwd')";}
+else echo "passwords dont match";
 
 $result = mysqli_query($db,$sql);
 
