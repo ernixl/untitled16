4b160b7b64aa479620eb8df61f73ecb686eb35e2
ernixl <ernixl@yahoo.com>
2017-04-17 19:05:51 +0100

    yes

--- login.php
+++ login.php
@@ -9,10 +9,10 @@ $result = mysqli_query($db, $sql);
 
 if ($result == true) {
     $row = mysqli_fetch_array($result);
-    if ($row['admin'] == 0) {
-        header("location: homepage.php");
-    } elseif ($row['admin'] == 1) {
-        header("location:admin.php");
+    if ($row['admin'] == 1) {
+        header("location: admin.php");
+    } elseif ($row['admin'] == 0) {
+        header("location:homepage.php");
     }
 } else
     echo "invalid password and username";
