5989c51f9d0c87b9f7c1ad44a0477c765046b6aa
ernixl <ernixl@yahoo.com>
2017-04-17 21:39:28 +0100

    yes

--- login.php
+++ login.php
@@ -1,23 +1,24 @@
 <?php
 
+$username=$_POST['username'];
+$password=$_POST['password'];
 
 include 'connect.php';
 
+$sql ="SELECT uid FROM login WHERE username='$username' and password='$password'";
 
-$username = $_POST['username'];
-$password = $_POST['password'];
+    $result =mysqli_query($db,$sql);
 
 
-$result = mysqli_query("SELECT id FROM login WHERE username='$username' and password='$password'")
 
-or die("failed to query database" . mysqli_error());
+
 
 $row = mysqli_fetch_array($result);
 
 if ($row['username'] == $username && $row['password'] == $password) {
-    header("Location: homepage.php");
+header("Location: homepage.php");
 
 } else {
-    echo "login failed";
+echo "login failed";
 }
 ?>
