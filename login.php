f4dff5058709c4002f2e237dca241b9f08c7ec45
ernixl <ernixl@yahoo.com>
2017-04-17 21:18:00 +0100

    yes

--- login.php
+++ login.php
@@ -1,16 +1,23 @@
 <?php
 
-$uid = $_POST['uid'];
-$pwd = $_POST['pwd'];
 
 include 'connect.php';
 
 
-$result = mysqli_query(" SELECT *  FROM login WHERE uid = '$uid' and pwd = '$pwd' ") or die("failed to query database".mysqli_error());
+$uid = $_POST['uid'];
+$pwd = $_POST['pwd'];
+
+
+$result = mysqli_query(" SELECT *  FROM login WHERE uid = '$uid' and pwd = '$pwd' ")
+
+or die("failed to query database" . mysqli_error());
 
 $row = mysqli_fetch_array($result);
 
-if ($row['uid']== $uid and $row['pwd']== $pwd){header("Location: homepage.php");
+if ($row['uid'] == $uid and $row['pwd'] == $pwd) {
+    header("Location: homepage.php");
 
-}else {echo "login failed";}
+} else {
+    echo "login failed";
+}
 ?>
