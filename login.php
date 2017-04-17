1e5604eeec3ca24323700c16298b19c0900191d7
ernixl <ernixl@yahoo.com>
2017-04-17 21:12:35 +0100

    yes

--- login.php
+++ login.php
@@ -1,19 +1,16 @@
 <?php
 
-include 'connect.php';
-
 $uid = $_POST['uid'];
 $pwd = $_POST['pwd'];
 
-$sql = " SELECT id FROM login WHERE uid='$uid' and pwd='$pwd' ";
+include 'connect.php';
 
-$result = mysqli_query($db,$sql);
+
+$result = mysqli_query(" SELECT *  FROM login WHERE uid = '$uid' and pwd = '$pwd' ") or die("failed to query database".mysqli_error());
 
 $row = mysqli_fetch_array($result);
 
-     if ( $row == 1) {
+if ($row['uid']== $uid and $row['pwd']== $pwd){header("Location: homepage.php");
 
-         header("Location: homepage.php");
-     }else echo "invalid password and username";
-
+}else {echo "login failed";}
 ?>
