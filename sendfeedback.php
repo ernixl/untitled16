33544d524412120bf34e7d0f2833bdf7c3ec62e4
ernixl <ernixl@yahoo.com>
2017-04-18 08:24:19 +0100

    yes

--- sendfeedback.php
+++ sendfeedback.php
@@ -5,15 +5,17 @@
     <link rel="stylesheet" href="style.css" type="text/css"/>
 </head>
 <body id="body">
+
 <h1>You can give feedback here</h1>
+
 <div id="form">
-    <form action=" feedback.php" method="post">
+    <form action="feedback.php" method="post">
         <input type="text" name="first" placeholder="Firstname"><br><br>
         <input type="text" name="last" placeholder="Lastname"><br><br>
         <input type="text" name="gname" placeholder="Groupname"><br><br>
         <input type="text" name="course" placeholder="Course Code"><br><br>
         <textarea name="feedback" placeholder="write your feedback here" id="" cols="30" rows="10"></textarea>
-        <button type="submit" id="submit">SignUp</button>
+        <button type="submit" id="submit">Submit feedback</button>
 
     </form>
 
