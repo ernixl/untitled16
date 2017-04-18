229a4008ccf3d98e98200c155d517d0dcd8f3f86
ernixl <ernixl@yahoo.com>
2017-04-18 07:57:05 +0100

    yes

--- sendfeedback.php
+++ sendfeedback.php
@@ -17,29 +17,29 @@
 
     </form>
 
-    <?php
-
-    $first = $_POST['first'];
-    $last = $_POST['last'];
-    $gname = $_POST['gname'];
-    $course = $_POST['course'];
-    $feedback = $_POST['feedback'];
-
-
-    include 'connect.php';
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        $sql = "INSERT INTO feedback (first, last, gname, course, feedback)
-    VALUES ('$first','$last','$gname','$course','$feedback')"; echo "feedback sent";
-    }else echo "error sending feedback";
-    $result = mysqli_query($db, $sql);
-
-    if ($result == true) {
-        echo "feedback sent,click <a href=\"homepage.php\">here</a> to return to homepage! ";
-    }
-
-    ?>
-
 </div>
+
+<?php
+
+$first = $_POST['first'];
+$last = $_POST['last'];
+$gname = $_POST['gname'];
+$course = $_POST['course'];
+$feedback = $_POST['feedback'];
+
+
+include 'connect.php';
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $sql = "INSERT INTO feedback (first, last, gname, course, feedback)
+    VALUES ('$first','$last','$gname','$course','$feedback')"; echo "feedback sent";
+}else echo "error sending feedback";
+$result = mysqli_query($db, $sql);
+
+if ($result == true) {
+    echo "feedback sent,click <a href=\"homepage.php\">here</a> to return to homepage! ";
+} else echo "i cannot";
+
+?>
 
 
 </body>
