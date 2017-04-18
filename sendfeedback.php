023f9f651a3612a905d5e7200432f56a63a6d10d
ernixl <ernixl@yahoo.com>
2017-04-18 07:51:30 +0100

    yes

--- sendfeedback.php
+++ sendfeedback.php
@@ -10,10 +10,9 @@
     <form action="<? echo $_SERVER['PHP_SELF'] ?>" method="post">
         <input type="text" name="first" placeholder="Firstname"><br><br>
         <input type="text" name="last" placeholder="Lastname"><br><br>
-        <input type="text" name="group" placeholder="Groupname"><br><br>
-        <input type="text" name="course" placeholder="Course id"><br><br>
+        <input type="text" name="gname" placeholder="Groupname"><br><br>
+        <input type="text" name="course" placeholder="Course Code"><br><br>
         <textarea name="feedback" placeholder="write your feedback here" id="" cols="30" rows="10"></textarea>
-        <input type="file" name="file" placeholder="file">
         <input type="submit" value="Send" id="submit">
 
     </form>
@@ -22,14 +21,15 @@
 
     $first = $_POST['first'];
     $last = $_POST['last'];
-    $group = $_POST['group'];
+    $gname = $_POST['gname'];
     $course = $_POST['course'];
     $feedback = $_POST['feedback'];
 
+
     include 'connect.php';
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        $sql = "INSERT INTO feedback (first, last, group, course, feedback)
-    VALUES ('$first','$last','$group','$course','$feedback')";
+        $sql = "INSERT INTO feedback (first, last, gname, course, feedback)
+    VALUES ('$first','$last','$gname','$course','$feedback')";
 
 
     }
