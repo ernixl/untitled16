71fcc85105df30c4863f75f285f658f6e138c89a
ernixl <ernixl@yahoo.com>
2017-04-19 22:35:06 +0100

    yes

--- index.php
+++ index.php
@@ -19,40 +19,39 @@
     <form action="index.php" method="post">
 
         Username: <input type="text" name="username"><br><br>
-        Password: <input type="password" name="password" ><br><br>
-        <button type="submit" name="register" >Register</button>
-        <button type="submit" name="submit" >Login</button><br><br>
+        Password: <input type="password" name="password"><br><br>
+        <button type="submit" name="register">Register</button>
+        <button type="submit" name="submit">Login</button>
+        <br><br>
 
     </form>
 
 
+    <?php
+    if (isset($_POST['submit'])) {
+        $username = $_POST['username'];
+        $password = $_POST['password'];
 
+        include 'connect.php';
 
-<?php
-if (isset($_POST['submit'])) {
-    $username = $_POST['username'];
-    $password = $_POST['password'];
+        $sql = "SELECT admin FROM login WHERE username='$username' and password='$password'";
 
-    include 'connect.php';
+        $result = mysqli_query($db, $sql);
+        $row = mysqli_fetch_array($result);
 
-    $sql = "SELECT admin FROM login WHERE username='$username' and password='$password'";
+        if (mysqli_num_rows($result) == 1 && $row['admin'] == 0) {
+            header("location: homepage.php");
+        } elseif (mysqli_num_rows($result) == 1 && $row['admin'] == 1) {
+            header("Location: admin.php");
+        } else
+            echo "<i>Incorrect username or password</i>";
+    }
 
-    $result = mysqli_query($db, $sql);
-    $row = mysqli_fetch_array($result);
+    if (isset($_POST['register'])) {
+        header('Location: register.php');
+    }
 
-    if (mysqli_num_rows($result) == 1 && $row['admin'] == 0) {
-        header("location: homepage.php");
-    } elseif (mysqli_num_rows($result) == 1 && $row['admin'] == 1) {
-        header("Location: admin.php");
-    } else
-        echo "<i>Incorrect username or password</i>";
-}
-
-if (isset($_POST['register'])) {
-    header('Location: register.php');
-}
-
-?>
+    ?>
 </div>
 </body>
 
