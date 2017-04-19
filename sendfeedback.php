b26e738177cd112df6ec35818bd4027747d8b22e
ernixl <ernixl@yahoo.com>
2017-04-19 18:08:25 +0100

    yes

--- sendfeedback.php
+++ sendfeedback.php
@@ -1,14 +1,35 @@
-<!DOCTYPE html>
 <html>
 <head>
-    <title>Register your details</title>
-    <link rel="stylesheet" href="stylebasic.css" type="text/css"/>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>North Lab</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="style.css">
+
+
+    <link href="bootstrap-material-design.css" rel="stylesheet">
+    <link href="ripples.min.css" rel="stylesheet">
 </head>
-<body id="body">
 
-<h1>You can give feedback here</h1>
+<body>
+<div class="container">
+    <header class="col-md-12">
+        <h1>Peer To Peer Evaluation</h1>
+        <h2>Give feedback</h2>
+    </header>
+    <nav class="col-md-12 navbar navbar-primary">
+        <div class="container-fluid">
+            <ul class="nav navbar-nav">
 
-<div id="form">
+                <li><a href="viewcoursework.php">View Coursework</a></li>
+                <li><a href="sendfeedback.php">Give Feedback</a></li>
+                <li><a href="upload_front.php">Upload File</a></li>
+
+    </nav>
+
+    <div id="form">
     <form action="sendfeedback.php" method="post">
         <input type="text" name="firstname" placeholder="Firstname"><br><br>
         <input type="text" name="lastname" placeholder="Lastname"><br><br>
