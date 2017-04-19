8e3225f2a469963d5447bf839636d0e25bb35b1b
ernixl <ernixl@yahoo.com>
2017-04-19 10:05:45 +0100

    yes

--- viewfeedback.php
+++ viewfeedback.php
@@ -5,13 +5,13 @@
 <?php
 include 'connect.php';
 
-$sql = "SELECT id, firstname, lastname, groupname, course, feed FROM feedback";
+$sql = "SELECT uid, firstname, lastname, groupname, course, feed FROM feedback";
 $result = mysqli_query($db, $sql);
 
 if (mysqli_num_rows($result)  > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "<br> ". $row["id"]. " - Nam: ". $row["firstname"]. " " . $row["lastname"] .$row["groupname"] .$row["course"] .$row["feed"] . "<br>";
+        echo "<br> ". $row["uid"]. " - Nam: ". $row["firstname"]. " " . $row["lastname"] .$row["groupname"] .$row["course"] .$row["feed"] . "<br>";
     }
 } else {
     echo "00 results";
