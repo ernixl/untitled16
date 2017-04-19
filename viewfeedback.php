9283b65038ba65fb5255f5ccfe942b7e97109486
ernixl <ernixl@yahoo.com>
2017-04-19 09:47:58 +0100

    yes

--- viewfeedback.php
+++ viewfeedback.php
@@ -8,7 +8,7 @@ include 'connect.php';
 $sql = "SELECT id, firstname, lastname, groupname, course, feed FROM feedback";
 $result = mysqli_query($db, $sql);
 
-if ($result->num_rows > 0) {
+if (mysqli_num_rows($result)  > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
         echo "<br> ". $row["id"]. " - Nam: ". $row["firstname"]. " " . $row["lastname"] .$row["groupname"] .$row["course"] .$row["feed"] . "<br>";
