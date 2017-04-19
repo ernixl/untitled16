4877b59400987fbd76d68806523ca2ed18eb980b
ernixl <ernixl@yahoo.com>
2017-04-19 22:17:54 +0100

    yes

--- homepage.php
+++ homepage.php
@@ -4,33 +4,22 @@
     <link rel="stylesheet" href="styling.css">
 </head>
 <body>
-<div>
-    <header >
-        <h1>Peer To Peer Evaluation</h1>
-    </header>
-    <nav >
-        <div>
-            <ul>
 
-                <li><a href="viewcoursework.php">View Coursework</a></li>
-                <li><a href="sendfeedback.php">Give Feedback</a></li>
-                <li><a href="upload_front.php">Upload File</a></li>
-
-    </nav>
-    <main>
-        <div>
-            <article>
-
-                <h3>Course Description</h3>
+<header>
+    <h1>Peer To Peer Evaluation</h1>
+</header>
 
 
+<ul>
+
+    <li><a href="viewcoursework.php">View Coursework</a></li>
+    <li><a href="sendfeedback.php">Give Feedback</a></li>
+    <li><a href="upload_front.php">Upload File</a></li>
+</ul>
+
+<h3>Course Description</h3>
 
 
-
-
-    </main>
-
-</div>
 </body>
 
 
