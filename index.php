6c79a7622ec79390872f2d4248530fe195f0e573
ernixl <ernixl@yahoo.com>
2017-04-19 18:44:26 +0100

    yes

--- index.php
+++ index.php
@@ -1,32 +1,43 @@
-<!doctype html >
 <html>
-<meta charset="utf-8">
 <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>North Lab</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="style.css">
 
-    <title>Peer to Peer Evaluation</title>
-    <link rel="stylesheet" href="stylebasic.css" type="text/css"/>
 
+    <link href="bootstrap-material-design.css" rel="stylesheet">
+    <link href="ripples.min.css" rel="stylesheet">
 </head>
 
-<body id="body">
+<body>
+<div class="container">
+    <header class="col-md-12">
+        <h1>Peer To Peer Evaluation</h1>
+        <h2>Web Application</h2>
+    </header>
+    <nav class="col-md-12 navbar navbar-primary">
+        <div class="container-fluid">
+            <ul class="nav navbar-nav">
 
-<h1>PHP Peer to Peer Evaluation </h1>
-
-<div id="form">
-
-    <h3>Enter your login information </h3>
+                <h3>Enter your login information </h3>
 
 
-    <form class="pure-form">
-        <fieldset>
-            <legend>Enter login information</legend>
+                <form class="pure-form">
+                    <fieldset>
+                        <legend>Enter login information</legend>
 
-            <input type="email" placeholder="Email">
-            <input type="password" placeholder="Password">
+                        <input type="email" placeholder="Email">
+                        <input type="password" placeholder="Password">
 
-            <button type="submit" class="pure-button pure-button-primary">Sign in</button>
-        </fieldset>
-    </form>
+                        <button type="submit" class="pure-button pure-button-primary">Sign in</button>
+                    </fieldset>
+                </form>
+
+    </nav>
 
 
     <form action="index.php" method="post">
