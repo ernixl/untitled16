4a4c387e7b6011caf3b7a58d4ddf5897ac5ea606
ernixl <ernixl@yahoo.com>
2017-04-19 07:56:14 +0100

    yes

--- coursework.php
+++ coursework.php
@@ -1,24 +1,6 @@
 <?php
 
-include 'connect.php';
-
-
-
-if (isset($_POST['submit'])) {
-    $coursename = $_POST['coursename'];
-    $title = $_POST['title'];
-    $description = $_POST['description'];
-    $sql = "INSERT INTO setcourse ( coursename, title, description)
-    VALUES ('$coursename','$title', $description)";
-}else{
-    echo "could not connect to database";
-}
-
-$result = mysqli_query($db, $sql);
-
-if ($result == true) {
-    header("Location: admin.php");
-}
+echo "hello";
 
 ?>
 
