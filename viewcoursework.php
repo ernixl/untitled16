47ae5e47b9a7b8921d81ea87c2402e3ee248e683
ernixl <ernixl@yahoo.com>
2017-04-19 11:14:49 +0100

    yes

--- viewcoursework.php
+++ viewcoursework.php
@@ -2,22 +2,7 @@
 <html>
 <body>
 
-<?php
-include 'connect.php';
-
-$sql = "SELECT coursename, description FROM setcourse";
-$result = mysqli_query($db, $sql);
-
-if (mysqli_num_rows($result) > 0) {
-
-    while ($row = $result->fetch_assoc()) {
-        echo "<b><i> Coursename: </i></b>" ." ". $row["coursename"] ." ". "<b><i> Description: </i></b>" ." ". $row["description"] . "<br>";
-    }
-} else {
-    echo "no results found";
-}
-
-?>
+hi
 
 </body>
 </html>
