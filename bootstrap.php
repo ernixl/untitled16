a8c0bce6252de73b4f1f262315f6c5039360bc3f
ernixl <ernixl@yahoo.com>
2017-04-19 16:46:25 +0100

    yes

--- bootstrap.php
+++ bootstrap.php
@@ -6,7 +6,7 @@
     <title>North Lab</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css">
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="style.css">
 
 
     <link href="bootstrap-material-design.css" rel="stylesheet">
