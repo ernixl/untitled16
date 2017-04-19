57f4818922a3429fe057d63c3a63c02600059255
ernixl <ernixl@yahoo.com>
2017-04-19 10:27:43 +0100

    yes

--- viewfeedback.php
+++ viewfeedback.php
@@ -11,7 +11,7 @@ $result = mysqli_query($db, $sql);
 if (mysqli_num_rows($result) > 0) {
     // output data of each row
     while ($row = $result->fetch_assoc()) {
-        echo "<br> " . $row["uid"] ." ". " <b><i>Name:</i></b> " ." ". $row["firstname"] ." ". "<b><i>LastName:</i></b> " ." ". $row["lastname"] ." ". "<b><i>Group:</i></b>" ." ". $row["groupname"] ." ". "<b><i>Course:</i></b>" ." ". $row["course"] ." ". "<b><i>Feedback:</i></b>" ." ". $row["feed"] . "<br>";
+        echo "<br> " . $row["uid"] ." ". " <b><i> Name: </i></b> " ." ". $row["firstname"] ." ". "<b><i> LastName: </i></b> " ." ". $row["lastname"] ." ". "<b><i> Group: </i></b>" ." ". $row["groupname"] ." ". "<b><i> Course: </i></b>" ." ". $row["course"] ." ". "<b><i> Feedback: </i></b>" ." ". $row["feed"] . "<br>";
     }
 } else {
     echo "00 results";
