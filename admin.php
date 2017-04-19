e5d04bf4c81d9379dfd381565403877bab954816
ernixl <ernixl@yahoo.com>
2017-04-20 00:24:00 +0100

    yes

--- admin.php
+++ admin.php
@@ -27,16 +27,6 @@
 </nav>
 
 
-<form action="admin.php" method="post">
-
-    <input type="submit" value="Home">
-    <input type="submit" value="View Feedback">
-    <input type="submit" value="View Uploads">
-    <input type="submit" value="Set Coursework">
-
-</form>
-
-
 <a href="index.php">Home</a>
 <a href="viewfeedback.php"> view feedbacks</a>
 <a href="viewupload.php">view uploads</a>
