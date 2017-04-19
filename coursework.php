3a983b8f1b9b60ea9b8852face9e7eb2d8f80168
ernixl <ernixl@yahoo.com>
2017-04-19 07:52:44 +0100

    yes

--- coursework.php
+++ coursework.php
@@ -9,6 +9,8 @@ $description = $_POST['description'];
 if (isset($_POST['submit'])) {
     $sql = "INSERT INTO setcourse ( coursename, title, description)
     VALUES ('$coursename','$title', $description)";
+}else{
+    echo "could not connect to database";
 }
 
 $result = mysqli_query($db, $sql);
