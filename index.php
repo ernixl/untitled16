dcd8e49766213196c084935f07aae6a0ce6d18c5
ernixl <ernixl@yahoo.com>
2017-04-19 13:58:37 +0100

    yes

--- index.php
+++ index.php
@@ -16,13 +16,44 @@
 
     <h3>Enter your login information</h3>
 
-    <form  action="login.php" method="post">
+    <form  action="index.php" method="post">
 
         <input  type="text" name="username" placeholder="Username"><br><br>
         <input  type="password" name="password" placeholder="Password"><br><br>
         <button type="submit" name="submit" id="submit" value="Login">Login</button>
+        <button type="submit" name="register" id="submit">Register</button>
 
     </form>
+
+
+    <?php
+    if (isset($_POST['submit'])) {
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        include 'connect.php';
+
+        $sql = "SELECT admin FROM login WHERE username='$username' and password='$password'";
+
+        $result = mysqli_query($db, $sql);
+        $row = mysqli_fetch_array($result);
+
+        if (mysqli_num_rows($result) == 1 && $row['admin'] == 0) {
+            header("location: homepage.php");
+        } elseif (mysqli_num_rows($result) == 1 && $row['admin'] == 1) {
+            header("Location: admin.php");
+        } else
+            echo "<i>error: Incorrect username or password</i>";
+    }
+
+    if (isset($_POST['register'])){
+        header('Location: register.php');
+    }
+
+    ?>
+
+
+
 
 </div>
 
