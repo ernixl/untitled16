a9474bec0a83b57ddf863e3885feab8e214011f2
ernixl <ernixl@yahoo.com>
2017-04-19 18:00:05 +0100

    yes

--- bootstrap.php
+++ bootstrap.php
@@ -24,7 +24,7 @@
             <ul class="nav navbar-nav">
 
                 <li><a href="viewcoursework.php">View Coursework</a></li>
-                <li><a href=<?php echo "bootstrap.php?feedback" ?>>Give Feedback</a></li>
+                <li><a href="sendfeedback.php">Give Feedback</a></li>
                 <li><a href="upload_front.php">Upload File</a></li>
 
     </nav>
@@ -32,69 +32,16 @@
         <div id="experimentContainer" class="row">
             <article class="col-md-12">
 
-                <h3>Experiment Name</h3>
-
-                <?php
-
-                if (isset($_GET['feedback'])){
-                ?>
-                <h1>You can give feedback here</h1>
-
-                <div id="">
-                    <form action="sendfeedback.php" method="post">
-                        <input type="text" name="firstname" placeholder="Firstname"><br><br>
-                        <input type="text" name="lastname" placeholder="Lastname"><br><br>
-                        <input type="text" name="groupname" placeholder="Groupname"><br><br>
-                        <input type="text" name="course" placeholder="Course Code"><br><br>
-                        <textarea name="feed" placeholder="write your feedback here" id="" cols="30"
-                                  rows="10"></textarea>
-                        <button type="submit" name="submit" id="submit">Submit</button>
-
-                    </form>
-
-                    <?php
-
-                    if (isset($_POST['submit'])) {
-
-                        include 'connect.php';
-
-                        $firstname = $_POST['firstname'];
-                        $lastname = $_POST['lastname'];
-                        $groupname = $_POST['groupname'];
-                        $course = $_POST['course'];
-                        $feed = $_POST['feed'];
+                <h3>Course Description</h3>
 
 
-                        if (empty($feed) || empty($course)) {
-                            echo "input all data";
-                        } else {
-                            $sql = "INSERT INTO feedback (firstname, lastname, groupname, course, feed)
-                          VALUES ('$firstname','$lastname','$groupname','$course','$feed')";
-                        }
-
-                        $result = mysqli_query($db, $sql);
-
-                        if ($result == true) {
-                            echo "<b>feedback sent successfully, click <a href=\"homepage.php\">here</a> to return to homepage</b>";
-                        } else echo "some error occured";
-                    }}
-                    ?>
-                </div>
 
 
-                ;
-                }
-
-                ?>
-            </article>
 
         </div><!--End of Experiment Container-->
     </main>
 
 </div>
-
-
-
 </body>
 
 
