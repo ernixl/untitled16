2f87ee51f7c9ef487db78bb7bbbe6854f888b1ed
ernixl <ernixl@yahoo.com>
2017-04-19 06:21:38 +0100

    yes

--- upload_front.php
+++ upload_front.php
@@ -8,11 +8,40 @@
 <h1>You can give upload files here</h1>
 <div id="form">
 
-<form action="upload_back.php" method="post" enctype="multipart/form-data">
+<form action="upload_front.php" method="post" enctype="multipart/form-data">
     Select image to upload:
     <input type="file" name="file" id="image"><br><br>
-    <button type="submit" name="submit">UPLOAD</button>
+    <button type="submit" name="file">UPLOAD</button>
 </form>
+
+    <?php
+    if (isset($_POST['submit'])) {
+        $file = $_FILES['file'];
+
+        $filename = $_FILES['file']['name'];
+        $fileTmpName = $_FILES['file']['tmp_name'];
+        $fileSize = $_FILES['file']['size'];
+        $fileError = $_FILES['file']['error'];
+        $fileType = $_FILES['file']['type'];
+
+        $fileExt = explode('.', $filename);
+        $fileActualExt = strtolower(end($fileExt));
+        $allowed = array('jpg', 'jpeg', 'png', 'pdf', 'docx');
+
+        if (in_array($fileActualExt, $allowed)) {
+            if ($fileError === 0) {
+                    $fileNameNew = uniqid('', true) . "." . $fileActualExt;
+                    $sql = "INSERT INTO image VALUES ('', '$fileNameNew', '$fileTmpName')";
+                    header("Location: homepage.php?uploadsuccess");
+            } else {
+                echo "you cannot upload files of this type due to error";
+            }
+
+        } else {
+            echo "you cannot upload files of this type";
+        }
+    }
+    ?>
 
 </body>
 </html>
