9ab5738589bcd7100c2e42a0137bc1a4f4d4ef37
ernixl <ernixl@yahoo.com>
2017-04-19 12:49:53 +0100

    yes

--- index.php
+++ index.php
@@ -36,24 +36,22 @@
 if (isset($_POST['submit'])) {
     $username = $_POST['username'];
     $password = $_POST['password'];
-    if (empty($username) || empty($password)) {
-        echo "username and password required";
 
-        include 'connect.php';
+    include 'connect.php';
 
-        $sql = "SELECT admin FROM login WHERE username='$username' and password='$password'";
+    $sql = "SELECT admin FROM login WHERE username='$username' and password='$password'";
 
-        $result = mysqli_query($db, $sql);
-        $row = mysqli_fetch_array($result);
+    $result = mysqli_query($db, $sql);
+    $row = mysqli_fetch_array($result);
 
-        if (mysqli_num_rows($result) == 1 && $row['admin'] == 0) {
-            header("location: homepage.php");
-        } elseif (mysqli_num_rows($result) == 1 && $row['admin'] == 1) {
-            header("Location: admin.php");
-        } else
-            echo "<i>error: Incorrect username or password</i>";
-    }
+    if (mysqli_num_rows($result) == 1 && $row['admin'] == 0) {
+        header("location: homepage.php");
+    } elseif (mysqli_num_rows($result) == 1 && $row['admin'] == 1) {
+        header("Location: admin.php");
+    } else
+        echo "<i>error: Incorrect username or password</i>";
 }
+
 ?>
 
 </body>
