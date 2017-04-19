1096e227a58c1707551f30303d08e1c7d9bcc7cb
ernixl <ernixl@yahoo.com>
2017-04-19 22:20:52 +0100

    yes

--- homepage.php
+++ homepage.php
@@ -9,13 +9,15 @@
     <h1>Peer To Peer Evaluation</h1>
 </header>
 
+<nav>
 
-<ul>
+    <ul>
+        <li><a href="viewcoursework.php">View Coursework</a></li>
+        <li><a href="sendfeedback.php">Give Feedback</a></li>
+        <li><a href="upload_front.php">Upload File</a></li>
+    </ul>
 
-    <li><a href="viewcoursework.php">View Coursework</a></li>
-    <li><a href="sendfeedback.php">Give Feedback</a></li>
-    <li><a href="upload_front.php">Upload File</a></li>
-</ul>
+</nav>
 
 <h3>Course Description</h3>
 
