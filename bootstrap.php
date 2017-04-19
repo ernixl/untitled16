a1ffc84906b57755ff63e77e34e06e07f3a8f982
ernixl <ernixl@yahoo.com>
2017-04-19 17:54:30 +0100

    yes

--- bootstrap.php
+++ bootstrap.php
@@ -16,7 +16,7 @@
 <body>
 <div class="container">
     <header class="col-md-12">
-        <h1>Peer To Peer Evaluat</h1>
+        <h1>Peer To Peer Evaluation</h1>
         <h2>Web Application</h2>
     </header>
     <nav class="col-md-12 navbar navbar-primary">
@@ -37,15 +37,64 @@
                 <?php
 
                 if (isset($_GET['feedback'])){
-                    header('Location: index.php');
+                ?>
+                <h1>You can give feedback here</h1>
+
+                <div id="form">
+                    <form action="sendfeedback.php" method="post">
+                        <input type="text" name="firstname" placeholder="Firstname"><br><br>
+                        <input type="text" name="lastname" placeholder="Lastname"><br><br>
+                        <input type="text" name="groupname" placeholder="Groupname"><br><br>
+                        <input type="text" name="course" placeholder="Course Code"><br><br>
+                        <textarea name="feed" placeholder="write your feedback here" id="" cols="30"
+                                  rows="10"></textarea>
+                        <button type="submit" name="submit" id="submit">Submit</button>
+
+                    </form>
+
+                    <?php
+
+                    if (isset($_POST['submit'])) {
+
+                        include 'connect.php';
+
+                        $firstname = $_POST['firstname'];
+                        $lastname = $_POST['lastname'];
+                        $groupname = $_POST['groupname'];
+                        $course = $_POST['course'];
+                        $feed = $_POST['feed'];
+
+
+                        if (empty($feed) || empty($course)) {
+                            echo "input all data";
+                        } else {
+                            $sql = "INSERT INTO feedback (firstname, lastname, groupname, course, feed)
+                          VALUES ('$firstname','$lastname','$groupname','$course','$feed')";
+                        }
+
+                        $result = mysqli_query($db, $sql);
+
+                        if ($result == true) {
+                            echo "<b>feedback sent successfully, click <a href=\"homepage.php\">here</a> to return to homepage</b>";
+                        } else echo "some error occured";
+                    }}
+                    ?>
+                </div>
+
+
+                ;
                 }
 
                 ?>
+            </article>
 
         </div><!--End of Experiment Container-->
     </main>
 
 </div>
+
+
+
 </body>
 
 
