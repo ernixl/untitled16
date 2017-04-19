f511e5da35150356b6608c2ca897d4a0964f92fd
ernixl <ernixl@yahoo.com>
2017-04-20 00:55:37 +0100

    yes

--- admin.php
+++ admin.php
@@ -41,13 +41,13 @@
                 <input type="text" name="coursename" placeholder="course name"><br><br>
                 <input type="text" name="title" placeholder="title"><br><br>
                 <textarea name="description" placeholder="description" id="" cols="30" rows="10"></textarea><br><br>
-                <button type="submit" name="submit">Submit</button>
+                <button type="submit" name="summit">Submit</button>
             </form>
 
         </article>
 
         <?php
-        if (isset($_POST['submit'])) {
+        if (isset($_POST['summit'])) {
             include 'connect.php';
 
             $coursename = $_POST['coursename'];
@@ -58,7 +58,7 @@
                 echo "<p><b><i>error 1: input all data</i></b></p>";
             } else {
                 $sql = "INSERT INTO setcourse( coursename, title, description)
-    VALUES ('$coursename','$title', '$description')";
+    VALUES ('$coursename','$title', '$description')"; echo "success";
             }
 
 
