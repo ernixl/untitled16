5f45d7678293a9ae4c3a246c76541c59469cf3d5
ernixl <ernixl@yahoo.com>
2017-04-20 21:19:09 +0100

    yes

--- admin.php
+++ admin.php
@@ -70,10 +70,39 @@
 
     <?php
     if (isset($_POST['view'])) {
-        header('Location: viewfeedback.php');
-    }
-?>
 
+        include 'connect.php';
+
+        $sql = "SELECT uid, firstname, lastname, course, feed FROM feedback";
+        $result = mysqli_query($db, $sql);
+    ?>
+    <table>
+        <tr>
+            <th>ID</th>
+            <th>FIRSTNAME</th>
+            <th>LASTNAME</th>
+            <th>COURSE</th>
+            <th>FEEDBACK</th>
+        </tr>
+
+        <?php
+        $result = mysqli_query($db, $sql);
+        If (mysqli_num_rows($result) > 0) {
+            while ($row = mysqli_fetch_array($result)) {
+                ?>
+                <tr>
+                    <td><?php echo $row['uid']; ?></td>
+                    <td><?php echo $row['firstname']; ?></td>
+                    <td><?php echo $row['lastname']; ?></td>
+                    <td><?php echo $row['course']; ?></td>
+                    <td><?php echo $row['feed']; ?></td>
+                </tr>
+                <?php
+            }
+        }
+        }
+        ?>
+    </table>
 
 
 
@@ -100,7 +129,8 @@
 
         $result = mysqli_query($db, $sql);
         If (mysqli_num_rows($result) > 0) {
-            while ($row = mysqli_fetch_array($result)) {$path = $row["image"];
+            while ($row = mysqli_fetch_array($result)) {
+                $path = $row["image"];
                 ?>
                 <tr>
                     <td><?php echo $row['id']; ?></td>
@@ -109,7 +139,7 @@
                 </tr>
                 <?php
             }
-        }
+            }
         }
         ?>
     </table>
