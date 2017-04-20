08789da51906f0c3bf2fd3deb664027052031ba6
ernixl <ernixl@yahoo.com>
2017-04-20 07:54:44 +0100

    yes

--- admin.php
+++ admin.php
@@ -84,19 +84,24 @@
         ?>
 
 
+        <?php
 
+        if (isset($_POST['files'])){
+            include 'connect.php';
 
+            $sql = "SELECT id, name, image FROM image";
+            $result = mysqli_query($db, $sql);
 
+            if (mysqli_num_rows($result) > 0) {
 
-
-
-
-
-
-
-
-
-
+                while ($row = $result->fetch_assoc()) {
+                    echo "<br> " . $row["id"] . " <b><i> Name : </i></b> " . $row["name"] . "<b><i> Image: </i></b> " . $row["image"] . "<br>";
+                }
+            } else {
+                echo "no results found";
+            }
+        }
+        ?>
     </div>
 </section>
 
