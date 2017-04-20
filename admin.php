11e59c39b269585438402b3de7da1ce9b59cf00c
ernixl <ernixl@yahoo.com>
2017-04-20 02:20:56 +0100

    yes

--- admin.php
+++ admin.php
@@ -15,7 +15,7 @@
 
 <nav>
 
-    <form action="setcoursework.php" method="post">
+    <form action="admin.php" method="post">
 
         <button type="submit" name="set">Set Coursework</button>
         <button type="submit" name="view">View feedback</button>
@@ -25,6 +25,49 @@
 
 
 </nav>
+
+<section>
+    <?php
+    if (isset($_POST['set'])) {
+        ?>
+        <div>
+            <form action="admin.php" method="post">
+                <input type="text" name="coursename" placeholder="course name"><br><br>
+                <input type="text" name="title" placeholder="title"><br><br>
+                <textarea name="description" placeholder="description" id="" cols="30" rows="10"></textarea>
+                <input type="submit" name="submit" value="submit">
+            </form>
+
+        </div>
+
+
+        <?php
+        if (isset($_POST['submit'])) {
+            include 'connect.php';
+
+            $coursename = $_POST['coursename'];
+            $title = $_POST['title'];
+            $description = $_POST['description'];
+
+            if (empty($coursename) || empty($title)) {
+                echo "<p><b><i>error 1: input all data</i></b></p>";
+            } else {
+                $sql = "INSERT INTO setcourse( coursename, title, description)
+    VALUES ('$coursename','$title', '$description')";
+            }
+
+
+            $result = mysqli_query($db, $sql);
+
+            if ($result == true) {
+                header("Location: admin.php");
+            } else echo "<p><b><i>error 2: failed</i></b></p>";
+        }
+    }
+
+    ?>
+    }
+
 
 </section>
 
