4b344d15878f9fb4fe26bf5d22908d2872dee5fe
ernixl <ernixl@yahoo.com>
2017-04-20 23:09:28 +0100

    yes

--- connect.php
+++ connect.php
@@ -1,11 +1,7 @@
 <?php
 session_start();
 
-if (isset($_SESSION['username'])){
 $db = mysqli_connect("127.0.0.1:57075","root","password","user");
-}else{
-    echo "Access denied";
-}
 
 if(!$db){
     die("Connection failed");
