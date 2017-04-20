fe17c97e97f62f6bde3ea7d2cd12d2ac0f106945
ernixl <ernixl@yahoo.com>
2017-04-20 07:50:16 +0100

    yes

--- admin.php
+++ admin.php
@@ -67,27 +67,36 @@
         <?php
         if (isset($_POST['view'])){
 
+            include 'connect.php';
 
+            $sql = "SELECT uid, firstname, lastname, groupname, course, feed FROM feedback";
+            $result = mysqli_query($db, $sql);
 
+            if (mysqli_num_rows($result) > 0) {
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+                while ($row = $result->fetch_assoc()) {
+                    echo "<br> " . $row["uid"] ." ". " <b><i> Name: </i></b> " ." ". $row["firstname"] ." ". "<b><i> LastName: </i></b> " ." ". $row["lastname"] ." ". "<b><i> Group: </i></b>" ." ". $row["groupname"] ." ". "<b><i> Course: </i></b>" ." ". $row["course"] ." ". "<b><i> Feedback: </i></b>" ." ". $row["feed"] . "<br>";
+                }
+            } else {
+                echo "no results found";
+            }
         }
         ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     </div>
 </section>
 
