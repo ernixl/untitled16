6383be4a1430b97c8ac20110c0885eaefd66b83d
ernixl <ernixl@yahoo.com>
2017-04-20 21:11:08 +0100

    yes

--- viewfeedback.php
+++ viewfeedback.php
@@ -1,9 +1,10 @@
 <?php
-
-if (isset($_POST['view'])){include 'connect.php';
+$value = 1;
+if ($value == 1){
+include 'connect.php';
 
 $sql = "SELECT uid, firstname, lastname, course, feed FROM feedback";
-$result = mysqli_query($db, $sql);}
+$result = mysqli_query($db, $sql);
 ?>
 <table>
     <tr>
@@ -17,16 +18,17 @@ $result = mysqli_query($db, $sql);}
     <?php
     $result = mysqli_query($db, $sql);
     If (mysqli_num_rows($result) > 0) {
-    while ($row = mysqli_fetch_array($result)) {
-    ?>
-    <tr>
-        <td><?php echo $row['uid']; ?></td>
-        <td><?php echo $row['firstname']; ?></td>
-        <td><?php echo $row['lastname']; ?></td>
-        <td><?php echo $row['course']; ?></td>
-        <td><?php echo $row['feed']; ?></td>
-    </tr>
-    <?php
+        while ($row = mysqli_fetch_array($result)) {
+            ?>
+            <tr>
+                <td><?php echo $row['uid']; ?></td>
+                <td><?php echo $row['firstname']; ?></td>
+                <td><?php echo $row['lastname']; ?></td>
+                <td><?php echo $row['course']; ?></td>
+                <td><?php echo $row['feed']; ?></td>
+            </tr>
+            <?php
+        }
     }
     }
     ?>
