584aaf117d67660a6a962282723ccea72ebfed9c
ernixl <ernixl@yahoo.com>
2017-04-20 09:07:39 +0100

    yes

--- homepage.php
+++ homepage.php
@@ -15,7 +15,7 @@
 
         <button type="submit" name="coursework">View Coursework</button>
         <button type="submit" name="feed">Give feedback</button>
-        <button type="submit" name="image">Upload file</button>
+        <button type="submit" name="file">Upload file</button>
 
     </form>
 
@@ -41,65 +41,22 @@
     }
     ?>
 
-    <?php
-    if (isset($_POST['feed'])) {
-        ?>
-        <article>
 
-            <form action="homepage.php" method="post">
-                <input type="text" name="firstname" placeholder="Firstname"><br><br>
-                <input type="text" name="lastname" placeholder="Lastname"><br><br>
-                <input type="text" name="groupname" placeholder="Groupname"><br><br>
-                <input type="text" name="course" placeholder="Course Code"><br><br>
-                <textarea name="feed" placeholder="write your feedback here" cols="30" rows="10"></textarea><br><br>
-                <button type="submit" name="submit">Submit</button>
-            </form>
-
-        </article>
-
-        <?php
-
-        if (isset($_POST['submit'])) {
-
-            include 'connect.php';
-
-            $firstname = $_POST['firstname'];
-            $lastname = $_POST['lastname'];
-            $groupname = $_POST['groupname'];
-            $course = $_POST['course'];
-            $feed = $_POST['feed'];
-
-
-            if (empty($feed) || empty($course)) {
-                echo "input all data";
-            } else {
-                $sql = "INSERT INTO feedback (firstname, lastname, groupname, course, feed)
-VALUES ('$firstname','$lastname','$groupname','$course','$feed')";
-            }
-
-            $result = mysqli_query($db, $sql);
-
-            if ($result == true) {
-                echo "<b>feedback sent successfully, click <a href=\"homepage.php\">here</a> to return to homepage</b>";
-            } else echo "some error occured";
-        }
-    }
-    ?>
 
     <?php
-    if (isset($_POST['image'])) {
+    if (isset($_POST['file'])) {
         ?>
 
         <div>
             <form action="homepage.php" method="post" enctype="multipart/form-data">
                 <b>Select file to upload:<br><br>
                     <input type="file" name="file" id="file"><br><br>
-                    <button type="submit" name="summit">Upload</button>
+                    <button type="submit" name="submit">Upload</button>
             </form>
         </div>
 
         <?php
-        if (isset($_POST['summit'])) {
+        if (isset($_POST['submit'])) {
             require("connect.php");
             $file = $_FILES['file'];
 
