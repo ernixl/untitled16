9e8d6a89f63f3ead267dad9097b98b8e807bb6da
ernixl <ernixl@yahoo.com>
2017-05-05 10:56:41 +0100

    yes

--- api.php
+++ api.php
@@ -5,6 +5,6 @@ include_once('connect.php');
 if (isset($_GET['uid'])) {
     $sql = "SELECT uid, first, last, FROM login";
     $result = mysqli_query($db, $sql);
-    If (mysqli_num_rows($result) > 0) echo "over the wall";
+    If (mysqli_num_rows($result) <= 0) echo "over the wall";
 
 } else echo "error";
