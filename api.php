13341e391c8eaef9bd381b0c729ae4672bb6b891
ernixl <ernixl@yahoo.com>
2017-05-05 10:11:53 +0100

    yes

--- api.php
+++ api.php
@@ -3,21 +3,5 @@ include_once('connect.php');
 
 
 if (isset($_GET['uid'])) {
-    $sql = "SELECT first, last, email, admin FROM login where uid = $uid";
-    $result = mysqli_query($db, $sql);
-
-
-    If (mysqli_num_rows($result) > 0) {
-        while ($row = mysqli_fetch_array($result)) {
-            ?>
-            <tr>
-                <td><?php echo $row['uid']; ?></td>
-                <td><?php echo $row['first']; ?></td>
-                <td><?php echo $row['last']; ?></td>
-                <td><?php echo $row['email']; ?></td>
-                <td><?php echo $row['admin']; ?></td>
-            </tr>
-            <?php
-        }
-    }
+    echo "yes";
 } else echo "error";
