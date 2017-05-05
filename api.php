bed674a2d1ec37de875cb27ccf7929dbadf85f8e
ernixl <ernixl@yahoo.com>
2017-05-05 08:36:12 +0100

    yes

---
+++ api.php
@@ -0,0 +1,22 @@
+<?php
+include_once('connect.php');
+
+$uid = isset($_GET['uid']) ? mysqli_real_escape_string($_GET['uid']) :  "";
+if(!empty($uid)){
+    $sql = mysqli_query("SELECT first, last, email FROM login where ID='$uid'");
+    $result =array();
+    while($r = mysqli_fetch_array($sql)){
+        extract($r);
+        $result[] = array("first" => $first, "last" => $last, "email" => $email, 'status' => $status);
+    }
+    $json = array("status" => 1, "info" => $result);
+}else{
+    $json = array("status" => 0, "msg" => "User ID not define");
+}
+
+
+
+header('Content-type: application/json');
+echo json_encode($json);
+
+
