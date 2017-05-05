63502dd4b8da99a0f11d5002d94403d4d0d7f64f
ernixl <ernixl@yahoo.com>
2017-05-05 10:59:22 +0100

    yes

--- api.php
+++ api.php
@@ -2,9 +2,9 @@
 include_once('connect.php');
 
 
-if (isset($_GET['uid'])) {
-    $sql = "SELECT uid, first, last, FROM login";
+if (isset($_GET['users'])) {
+    $sql = "SELECT uid, first, last, username FROM login";
     $result = mysqli_query($db, $sql);
-    If (mysqli_num_rows($result) <= 0) echo "over the wall";
+    If (mysqli_num_rows($result) > 0) echo "over the  entire wall";
 
 } else echo "error";
