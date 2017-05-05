c5c995033c504e166ddafd7aa425bf1b51dbe650
ernixl <ernixl@yahoo.com>
2017-05-05 11:10:31 +0100

    yes

--- api.php
+++ api.php
@@ -5,6 +5,8 @@ include('connect.php');
 if (isset($_GET['users'])) {
     $sql = "SELECT uid, first, last, username FROM login";
     $result = mysqli_query($db, $sql);
-    If (mysqli_num_rows($result) > 0) echo "over the  entire wall";
+    If (mysqli_num_rows($result) > 0);
+    header('Content-type: text/javascript');
+    echo json_encode($result);
 
 } else echo "error";
