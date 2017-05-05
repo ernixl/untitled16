ea728e532c865701520776ec503ceb840f0de4c3
ernixl <ernixl@yahoo.com>
2017-05-05 10:09:45 +0100

    yes

--- api.php
+++ api.php
@@ -1,22 +1,23 @@
 <?php
 include_once('connect.php');
 
-$uid = isset($_GET['uid']) ? mysqli_real_escape_string($_GET['uid']) :  "";
-if(!empty($uid)){
-    $sql = mysqli_query("SELECT first, last, email, admin FROM login where uid = $uid");
-    $result =array();
-    while($r = mysqli_fetch_array($sql)){
-        extract($r);
-        $result[] = array('first' => $first, 'last' => $last, 'email' => $email, 'admin' => $admin);
+
+if (isset($_GET['uid'])) {
+    $sql = "SELECT first, last, email, admin FROM login where uid = $uid";
+    $result = mysqli_query($db, $sql);
+
+
+    If (mysqli_num_rows($result) > 0) {
+        while ($row = mysqli_fetch_array($result)) {
+            ?>
+            <tr>
+                <td><?php echo $row['uid']; ?></td>
+                <td><?php echo $row['first']; ?></td>
+                <td><?php echo $row['last']; ?></td>
+                <td><?php echo $row['email']; ?></td>
+                <td><?php echo $row['admin']; ?></td>
+            </tr>
+            <?php
+        }
     }
-    $json = array('admin' => 1, 'info' => $result);
-}else{
-    $json = array('admin' => 0, 'msg' => "User ID not define");
-}
-
-
-
-header('Content-type: application/json');
-echo json_encode($json);
-
-
+} else echo "error";
