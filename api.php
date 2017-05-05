03e14359c832596f01123bdb64d5ca4ecd7bf7b2
ernixl <ernixl@yahoo.com>
2017-05-05 10:36:12 +0100

    yes

--- api.php
+++ api.php
@@ -4,5 +4,10 @@ include_once('connect.php');
 
 if (isset($_GET['uid'])) {$sql = "SELECT uid, first, last, FROM login";
     $result = mysqli_query($db, $sql);
-    echo "nono";
+    $results = array();
+    while ($info = mysqli_fetch_array($result)){
+        $results[] = $info;
+    }
+    $results = json_encode($results);
+    echo $_GET['uid'] . '(' . $results . ')';
 } else echo "error";
