44fc7302046338d8481b0c7f48f905aff87a2f0f
ernixl <ernixl@yahoo.com>
2017-05-05 09:56:58 +0100

    yes

--- api.php
+++ api.php
@@ -3,11 +3,11 @@ include_once('connect.php');
 
 $uid = isset($_GET['uid']) ? mysqli_real_escape_string($_GET['uid']) :  "";
 if(!empty($uid)){
-    $sql = mysqli_query("SELECT first, last, email, admin FROM login where uid='$uid'");
+    $sql = mysqli_query("SELECT first, last, email, admin FROM login where uid = $uid");
     $result =array();
     while($r = mysqli_fetch_array($sql)){
         extract($r);
-        $result[] = array('first' => $first, 'last' => $last, 'email' => $email, 'admin' => $status);
+        $result[] = array('first' => $first, 'last' => $last, 'email' => $email, 'admin' => $admin);
     }
     $json = array('admin' => 1, 'info' => $result);
 }else{
