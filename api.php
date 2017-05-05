e6ebf59ffe446637cbcffd3c415caf3b2864475f
ernixl <ernixl@yahoo.com>
2017-05-05 11:20:29 +0100

    yes

--- api.php
+++ api.php
@@ -4,8 +4,15 @@ include('connect.php');
 
 if (isset($_GET['users'])) {
     $sql = "SELECT uid, first, last, username FROM login";
-    $result = mysqli_query($db, $sql);
-    If (mysqli_num_rows($result) > 0);
+    $qur = mysqli_query($db, $sql);
+    If (mysqli_num_rows($qur) > 0);
+
+    $result =array();
+    while($r = mysqli_fetch_array($qur)){
+        extract($r);
+        $result[] = array("first" => $first, "last" => $last, "email" => $email, 'username' => $username);
+    }
+
     header('Content-type: text/javascript');
     echo json_encode($result);
 
