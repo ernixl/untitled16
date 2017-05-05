4ef62fab1a1339f1df1de5f4989817e121842357
ernixl <ernixl@yahoo.com>
2017-05-05 13:38:50 +0100

    yes

--- api.php
+++ api.php
@@ -5,15 +5,16 @@ include('connect.php');
 if (isset($_GET['allusers'])) {
     $sql = "SELECT uid, first, last,email, username FROM login";
     $qur = mysqli_query($db, $sql);
-    If (mysqli_num_rows($qur) > 0);
+    If (mysqli_num_rows($qur) > 0) ;
 
-    $result =array();
-    while($r = mysqli_fetch_array($qur)){
+    $result = array();
+    while ($r = mysqli_fetch_array($qur)) {
         extract($r);
         $result[] = array(first => $first, last => $last, email => $email, username => $username);
     }
 
     header('Content-type: text/javascript');
-    echo json_encode($result); "<br>";
+    echo json_encode($result)."<br>";
 
-} else echo "invalid method";
+
+} else echo "invalid meth";
